24ab1cb6e0d395bc0c89622f5dc12a5049c832b6
Andre Garcia <andre.f.garcia@inesctec.pt>
2020-03-15 23:57:03 +0000

    added sources

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
 from MyHtmlParser import MyHtmlParser
+from sources import data
 
 if __name__ == '__main__':
-
-    data = [
-        {
-            "url": "https://transparency.entsoe.eu/load-domain/r2/totalLoadR2/show?name=&defaultValue=true&"
-                   "viewType=TABLE&areaType=BZN&atch=false&dateTime.dateTime={datetime}|UTC|DAY&"
-                   "biddingZone.values=CTY|10YPT-REN------W!BZN|10YPT-REN------W&"
-                   "dateTime.timezone=UTC&dateTime.timezone_input=UTC",
-            "period": ("2013-2-1", "2020-3-12"),
-            "dataset": "Portugal load"
-        },
-        {
-            "url": "https://transparency.entsoe.eu/load-domain/r2/totalLoadR2/show?name=&defaultValue=false&"
-                   "viewType=TABLE&areaType=BZN&atch=false&dateTime.dateTime={datetime}|UTC|DAY&"
-                   "biddingZone.values=CTY|10YES-REE------0!BZN|10YES-REE------0&dateTime.timezone=UTC&d"
-                   "ateTime.timezone_input=UTC",
-            "period": ("2013-2-1", "2020-3-12"),
-            "dataset": "Spain load"
-        }
-    ]
 
     for d in data:
 
---
+++ sources.py
@@ -0,0 +1,18 @@
+data = [
+    {
+        "url": "https://transparency.entsoe.eu/load-domain/r2/totalLoadR2/show?name=&defaultValue=true&"
+               "viewType=TABLE&areaType=BZN&atch=false&dateTime.dateTime={datetime}|UTC|DAY&"
+               "biddingZone.values=CTY|10YPT-REN------W!BZN|10YPT-REN------W&"
+               "dateTime.timezone=UTC&dateTime.timezone_input=UTC",
+        "period": ("2013-2-1", "2020-3-12"),
+        "dataset": "Portugal load"
+    },
+    {
+        "url": "https://transparency.entsoe.eu/load-domain/r2/totalLoadR2/show?name=&defaultValue=false&"
+               "viewType=TABLE&areaType=BZN&atch=false&dateTime.dateTime={datetime}|UTC|DAY&"
+               "biddingZone.values=CTY|10YES-REE------0!BZN|10YES-REE------0&dateTime.timezone=UTC&d"
+               "ateTime.timezone_input=UTC",
+        "period": ("2013-2-1", "2020-3-12"),
+        "dataset": "Spain load"
+    }
+]
